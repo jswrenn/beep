da0d566b6200fd3b43af51ed51245b2b49ca309c
Jack Wrenn <me@jswrenn.com>
2017-08-27 14:01:14 -0400

    fix example

--- examples/beep.rs
+++ examples/beep.rs
@@ -4,7 +4,6 @@ extern crate dimensioned;
 use std::{thread, time};
 use beep::beep;
 use dimensioned::si::Hertz;
-use std::{thread, time};
 
 fn main() {
 
@@ -13,15 +12,14 @@ fn main() {
   let E = Hertz::new(2673.0f64);
   let F = Hertz::new(2673.0f64);
   let G = Hertz::new(3136.0f64);
-
-  let silence = Hertz::new(0.0f64);
+  let rest = Hertz::new(0.0f64);
 
   let note_length = time::Duration::from_millis(250);
 
   let song =
-    [E, D, C, D, E, E, E, silence,
-     D, D, D, silence,
-     E, G, G, silence,
+    [E, D, C, D, E, E, E, rest,
+     D, D, D, rest,
+     E, G, G, rest,
      E, D, C, D, E, E, E,
      E, D, D, E, D, C];
 
@@ -30,5 +28,5 @@ fn main() {
     thread::sleep(note_length);
   }
 
-  beep(silence);
+  beep(rest);
 }
